e88140893ffb2a48585147aa4328cc3ab3af4557
John Toro <joaltoroc@gmail.com>
2026-04-28 21:49:07 -0500

    chore: setup husky + typescript check

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,32 @@
 import { defineConfig, passthroughImageService } from 'astro/config';
 import compression from 'vite-plugin-compression';
+import sitemap from '@astrojs/sitemap';
+import robotsTxt from 'astro-robots-txt';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://jatc.co',
   prefetch: true,
+  integrations: [
+    sitemap({
+      i18n: {
+        defaultLocale: 'es',
+        locales: {
+          es: 'es-CO',
+          en: 'en-US',
+        },
+      },
+    }),
+    robotsTxt({
+      sitemap: 'https://jatc.co/sitemap-index.xml',
+      policy: [
+        {
+          userAgent: '*',
+          allow: '/',
+        },
+      ],
+    }),
+  ],
   image: {
     service: passthroughImageService()
   },
